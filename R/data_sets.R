364843e60f3dec474b26ff53b1934dae47798a34
Aleksander Chlebowski <olobiolo@gmail.com>
2021-10-04 23:48:34 +0200

    document data sets

---
+++ R/data_sets.R
@@ -0,0 +1,15 @@
+
+#' part of Edgar Anderson's Iris Data
+#'
+#' A small subset of the built-in \code{datasets::iris} data set, containing
+#' 5 randomly selected observations from each species.
+#'
+"iris"
+
+
+
+#' modified version of the Motor Trend Car Road Tests
+#'
+#' The built-in \code{datasets::mtcars} data set with added "make" and "model" columns.
+#'
+"mtcars"
